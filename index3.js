d1cc676fbb8e8d362f90ddcfc9e097316cedca56
Arushi Tandon <135385191+ArushiTandon@users.noreply.github.com>
2024-08-13 03:08:57 +0530

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,14 @@
+const header = document.getElementById('header');
+header.style.color = 'orange';
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '3px solid orange';
+
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World';
+mainHeading.style.color = 'orange';
+
+const heading = document.getElementById('basket-heading');
+heading.style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</p>';
